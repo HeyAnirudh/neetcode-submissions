77447f283ea884a92d94c04e1ad78b8d7bffa0d2
Anirudh soni <anirudh90hyd@gmail.com>
2026-05-11 12:07:11 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x: x.start)
+        for i in range(0, len(intervals) - 1):
+            if intervals[i].end > intervals[i+1].start:
+                return False
+        return True
